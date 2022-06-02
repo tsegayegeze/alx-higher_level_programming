16b33a50cd6b472cb960708b24aaee47f5f6c3a2
tsegayegeze <tsegayegeze@gmail.com>
2022-06-02 22:29:07 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if total <= 1:
         print("0 argument.")
     else:
-        if total == 1:
+        if total == 2:
             print("{:d} argument:".format(total - 1))
         else:
             print("{:d} arguments:".format(total - 1))
