59c2ed37142597bb79fa692a8b1a3b04681c9e1e
tsegayegeze <tsegayegeze@gmail.com>
2022-06-02 21:45:09 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    print(f"{len(argv)} arguments:")
-    for i, arg in enumerate(argv):
-        if i == 0:
-            continue
-        print(f"{i}: {arg}")
+    import sys
+    total = len(sys.argv)
+    if total <= 1:
+        print("0 argument.")
+    else:
+        if total == 2:
+            print("{:d} argument:".format(total - 1))
+        else:
+            print("{:d} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))
