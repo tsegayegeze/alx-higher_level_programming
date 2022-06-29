47cf03115423ee71c1b5698be968d0531eaf7f75
tsegayegeze <tsegayegeze@gmail.com>
2022-06-29 23:03:44 +0300

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""locked class module"""
+
+
 class LockedClass:
-
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    """locking the __slots__"""
+    __slots__ = 'first_name'
