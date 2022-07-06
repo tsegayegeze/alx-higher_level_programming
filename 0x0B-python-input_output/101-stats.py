2a106f7e572055bf698f405780135457a972bd92
tsegayegeze <tsegayegeze@gmail.com>
2022-07-06 05:46:55 +0300

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 101-stats.py
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
@@ -17,6 +16,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
