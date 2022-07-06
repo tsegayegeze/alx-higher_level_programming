3743b9c1d3002bf02d07ed5c0edbcb54ef8e2fec
tsegayegeze <tsegayegeze@gmail.com>
2022-07-06 04:21:05 +0300

    2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+"""Module 2-read_lines.
+Reads a certain number of lines from a file.
+"""
+
+
 def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
+    """Reads and prints nb_lines lines from filename.
+    Args:
+        - filename: name of the file
+        - nb_lines: number of lines to read
+    """
+
+    with open(filename) as f:
+        i = 0
+        count = 0
+        for line in f:
+            count += 1
+        f.seek(0)
+        if nb_lines <= 0 or nb_lines >= count:
+            read_text = f.read()
+            print(read_text, end="")
         else:
-            for line in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+            for line in f:
+                print(line, end='')
+                i += 1
+                if i == nb_lines:
+                    break
